637c1282bae59b17b8615a9de41dac934cc7e246
Omaam <omama.tomoki@gmail.com>
2022-10-31 09:48:29 +0900

    Modefy

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,25 +1,40 @@
 """Test for model module.
 """
+import math
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 import tsmodels
 
-ar_coefs = [1.55, -0.81]
+
+def plot_power_spectrum(armodel):
+    freqs = np.linspace(0, 0.5, 1000)
+    powers = armodel.compute_power_spectrum(freqs)
+    fig, ax = plt.subplots()
+    ax.plot(freqs, powers)
+    ax.set_yscale("log")
+    # plt.show()
+    plt.close()
 
 
-def test_armodel():
+def plot_impulse_response_function(armodel):
+    irf = armodel.compute_impulse_response_function(20)
+    fig, ax = plt.subplots()
+    ax.plot(irf)
+    ax.set_ylim(-1, 2)
+    # plt.show()
+    plt.close()
+
+
+def example():
     armodel = tsmodels.ARModel(ar_coefs)
 
-    # freqs = np.linspace(0, 0.5, 1000)
-    # powers = armodel.compute_power_spectrum(freqs)
+    plot_power_spectrum(armodel)
 
-    irf = armodel.compute_impulse_response_function(20)
-    print(irf)
-
-    plt.plot(irf)
-    plt.ylim(-1, 2)
-    plt.show()
+    plot_impulse_response_function(armodel)
 
 
 if __name__ == "__main__":
-    test_armodel()
+    ar_coefs = [0.9*math.sqrt(3), -0.81]
+    example()
