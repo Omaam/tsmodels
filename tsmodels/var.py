df9525326933ce3488a719991e97d75802e5972a
Omaam <omama.tomoki@gmail.com>
2023-01-26 11:00:01 +0900

    modefy coherence

--- tsmodels/var.py
+++ tsmodels/var.py
@@ -18,9 +18,8 @@ class VARAnalyzer:
         a0 = np.diag(np.repeat(-1, self.num_ts))
         arcoef_a0 = np.insert(self.arcoef, 0, a0, axis=0)
 
-        ms = np.arange(self.arorder+1)
-        phases = -2j*np.pi*freqs[:, None, None, None] * ms[:, None, None]
-        arcoef_fourier = arcoef_a0 * np.exp(phases)
+        phases = -2j * np.pi * freqs[:, None] * np.arange(self.arorder + 1)
+        arcoef_fourier = arcoef_a0 * np.exp(phases)[:, :, None, None]
 
         arcoef_fourier = arcoef_fourier.sum(axis=1)
 
@@ -41,17 +40,21 @@ class VARAnalyzer:
         for i in range(len(freqs)):
             arcoef_fourier_inv_conjugate[i] = np.matrix(
                 self.arcoef_fourier_inv[i]).getH()
-        cross_spectrum = self.arcoef_fourier_inv @ self.obs_noise_matrix \
-            @ arcoef_fourier_inv_conjugate
+
+        cross_spectrum = self.arcoef_fourier_inv
+        cross_spectrum = cross_spectrum @ self.obs_noise_matrix
+        cross_spectrum = cross_spectrum @ arcoef_fourier_inv_conjugate
         self.cross_spectrum = cross_spectrum
+
         return cross_spectrum
+
+    def _check_computation_crossspectrum(self):
+        if self.cross_spectrum is None:
+            raise AttributeError("you must do 'compute_cross_spectrum'")
 
     @property
     def amplitude_spectrum(self):
-        if self.cross_spectrum is None:
-            raise AttributeError(
-                "you must do 'compute_cross_spectrum' before "
-                "getting amplitude spectrum.")
+        self._check_computation_crossspectrum()
         return np.abs(self.cross_spectrum)
 
     @property
@@ -61,43 +64,28 @@ class VARAnalyzer:
         TODO:
             * This DON'T work well. Must be repaired.
         """
-        if self.cross_spectrum is None:
-            raise AttributeError(
-                "you must do 'compute_cross_spectrum' before "
-                "getting coherency.")
+        self._check_computation_crossspectrum()
 
-        js = np.arange(3)
-        ks = np.arange(3)
+        alpha_jk = self.amplitude_spectrum**2
+        p_jj = np.real(np.diagonal(self.cross_spectrum, axis1=1, axis2=2))
+        p_kk = np.real(np.diagonal(self.cross_spectrum, axis1=1, axis2=2))
+        coherency = alpha_jk / p_jj[:, :, None] / p_kk[:, None, :]
 
-        # print(np.real(self.cross_spectrum[:, js[:, None], js[:, None]])[0])
-        # print(np.real(self.cross_spectrum[:, ks[None, :], ks[None, :]])[0])
-        upper = self.amplitude_spectrum**2
-        under = np.real(self.cross_spectrum[:, ks[None, :], ks[None, :]]) \
-            * np.real(self.cross_spectrum[:, js[:, None], js[:, None]])
-        # print(upper[0])
-        # print(under[0])
-        # print(coherency)
-        coherency = upper / under
         return coherency
 
     @property
     def frequency(self):
-        if self.cross_spectrum is None:
-            raise AttributeError(
-                "you must do 'compute_cross_spectrum' before "
-                "getting frequency.")
+        self._check_computation_crossspectrum()
         return self.freqs
 
     @property
     def phase_spectrum(self):
-        if self.cross_spectrum is None:
-            raise AttributeError(
-                "you must do 'compute_cross_spectrum' before "
-                "getting phase spectrum.")
+        self._check_computation_crossspectrum()
         return np.angle(self.cross_spectrum)
 
 
 def main():
+
     np.random.seed(0)
     arcoef = np.random.normal(0, 1, (8, 3, 3))
     obs_noise_matrix = np.diag(np.random.normal(0, 1, 3))
