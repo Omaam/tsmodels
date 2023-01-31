f9a216b99c3846816f1bc944f4b7cda17c9f7e54
Omaam <omama.tomoki@gmail.com>
2023-01-31 17:26:59 +0900

    rename function name

--- tsmodels/var.py
+++ tsmodels/var.py
@@ -12,24 +12,13 @@ class VARAnalyzer:
         self.W = np.diag(sigma2)
         self.P = None
 
-    def compute_arcoef_fourier(self, freqs):
-        a0 = np.diag(np.repeat(-1, self.num_ts))
-        arcoef_a0 = np.insert(self.arcoef, 0, a0, axis=0)
-
-        phases = -2j * np.pi * freqs[:, None] * np.arange(self.arorder + 1)
-        A = arcoef_a0 * np.exp(phases)[:, :, None, None]
-        A = A.sum(axis=1)
-
-        self.A = A
-        return A
-
     def compute_cross_spectrum(self, freqs=None):
         if freqs is None:
             freq_edges = np.linspace(0, 0.5, 101, endpoint=True)
             freqs = (freq_edges[1:] + freq_edges[:-1]) / 2
         self.freqs = freqs = np.asarray(freqs)
 
-        A = self.compute_arcoef_fourier(freqs)
+        A = self._compute_arcoef_fourier(freqs)
         B = np.linalg.inv(A)
         arcoef_fourier_inv_conjugate = np.conjugate(np.transpose(
             B, axes=[0, 2, 1]))
@@ -44,6 +33,17 @@ class VARAnalyzer:
     def _check_computation_crossspectrum(self):
         if self.P is None:
             raise AttributeError("you must do 'compute_cross_spectrum'")
+
+    def _compute_arcoef_fourier(self, freqs):
+        a0 = np.diag(np.repeat(-1, self.num_ts))
+        arcoef_a0 = np.insert(self.arcoef, 0, a0, axis=0)
+
+        phases = -2j * np.pi * freqs[:, None] * np.arange(self.arorder + 1)
+        A = arcoef_a0 * np.exp(phases)[:, :, None, None]
+        A = A.sum(axis=1)
+
+        self.A = A
+        return A
 
     @property
     def amplitude_spectrum(self):
